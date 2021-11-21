ce9f2ad1267806493f0ffab2d3d2696466ad6b1e
Scott Miller <pwshaddict@scottmiller.dev>
2021-11-21 09:31:44 -0500

    adding some logging

--- dev_windows_setup.ps1
+++ dev_windows_setup.ps1
@@ -1,10 +1,18 @@
-
+$InformationPreference = 'Continue'
 # Install powershell modules 
 $modules = @(
     'posh-git'
-    ,'oh-my-posh'
+    , 'oh-my-posh'
 
 )
 foreach ($mod in $modules) {
-     Install-Module -Name $mod -AllowClobber -Force
+    Write-Information -MessageData "Insatlling/upgrading module: [ $mod ]"
+    if (Get-InstalledModule -Name $mod) {
+        Write-Information -MessageData "updating [ $mod ] if needed."
+        Update-Module -Name $mod 
+    }
+    else {
+        Write-Information -MessageData "Installing: [ $mod ]"
+        Install-Module -Name $mod -AllowClobber -Force
+    }
 }

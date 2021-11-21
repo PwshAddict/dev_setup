8011db3594ef9c1411c657143bc52f9c841f2b1d
Scott Miller <pwshaddict@scottmiller.dev>
2021-11-21 10:59:04 -0500

    add readline stuff to profile

--- pwsh_profile.ps1
+++ pwsh_profile.ps1
@@ -2,3 +2,23 @@
 import-module posh-git
 Set-PoshPrompt -Theme pixelrobots
 
+
+if ($Host.Name -match "ConsoleHOst|Visual Studio Code Host") {
+    Import-Module -Name PSReadline
+}
+
+Set-PSReadLineKeyHandler -Chord Tab -Function MenuComplete
+Set-PSReadLineKeyHandler -key DownArrow -Function HistorySearchForward
+Set-PSReadLineKeyHandler -key UpArrow -Function HistorySearchBackward
+$psRlo = @{
+    HistorySearchCursorMovesToEnd = $true
+    BellStyle = 'None' 
+    PredictionSource = 'History'
+}
+
+Set-PSReadLineOption @psRlo
+
+# make git log easy to read
+Function graph {
+    git log --all --decorate --oneline --grash
+}

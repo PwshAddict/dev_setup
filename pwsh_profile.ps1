bc91d1a9b35b56cff758263d92bd48220fec2224
Scott Miller <pwshaddict@scottmiller.dev>
2021-11-21 13:23:01 -0500

    version dependant stuff

--- pwsh_profile.ps1
+++ pwsh_profile.ps1
@@ -2,6 +2,13 @@
 import-module posh-git
 Set-PoshPrompt -Theme pixelrobots
 
+if ($IsWindows -or [string]::IsNullOrEmpty($IsWindows)) {
+    # Chocolatey profile
+    $ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+    if (Test-Path($ChocolateyProfile)) {
+        Import-Module "$ChocolateyProfile"
+    }
+}
 
 if ($Host.Name -match "ConsoleHOst|Visual Studio Code Host") {
     Import-Module -Name PSReadline
@@ -12,8 +19,12 @@ Set-PSReadLineKeyHandler -key DownArrow -Function HistorySearchForward
 Set-PSReadLineKeyHandler -key UpArrow -Function HistorySearchBackward
 $psRlo = @{
     HistorySearchCursorMovesToEnd = $true
-    BellStyle = 'None' 
-    PredictionSource = 'History'
+    BellStyle                     = 'None' 
+   
+}
+
+if ($PSVersionTable.PSVersion -ge [version]'7.2') {
+    $psRlo.Add('PredictionSource', 'History')
 }
 
 Set-PSReadLineOption @psRlo

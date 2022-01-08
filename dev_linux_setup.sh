8772fb70dadcc5238b1246334c8d2391fcc3b88e
Scott Miller <pwshaddict@scottmiller.dev>
2022-01-08 11:29:31 -0500

    add terraform install to linux

--- dev_linux_setup.sh
+++ dev_linux_setup.sh
@@ -32,3 +32,10 @@ cd ~
 FOLDER=~/sources/
 [ ! -d $FOLDER ] && mkdir sources/
 
+# install terraform
+# https://learn.hashicorp.com/tutorials/terraform/install-cli
+sudo apt-get update && sudo apt-get install -y gnupg software-properties-common curl
+curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
+
+sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+sudo apt-get update && sudo apt-get install terraform
